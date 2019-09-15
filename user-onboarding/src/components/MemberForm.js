68c9699565074afcaac77bba5ed4adf2ca7dd309
Allie Robinson <allie-rae@Allies-MacBook.local>
2019-09-15 12:09:38 -0500

    Form validation functional. Post request successful.

--- user-onboarding/src/components/MemberForm.js
+++ user-onboarding/src/components/MemberForm.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withFormik, Form, Field } from 'formik';
+import * as yup from 'yup';
+import axios from 'axios';
 
-const MemberForm = ({ status }) => {
+const MemberForm = ({ errors, touched, status }) => {
+
+       const [members, setMembers] = useState([]);
+
+       useEffect(() => {
+              if (status) {
+                     setMembers([...members, status])
+              }
+       }, [status])
 
        return (
        <Form>
+              {touched.name && errors.name && <p className="error">{errors.name}</p>}
               <Field name="name" 
                      type="text" 
                      placeholder="Full Name" 
                      /> 
+              {touched.email && errors.email && <p className="error">{errors.email}</p>}
               <Field name="email" 
                      type="email" 
                      placeholder="Email" 
                      /> 
+              {touched.password && errors.password && <p className="error">{errors.password}</p>}
               <Field name="password" 
                      type="password" 
                      placeholder="Password" 
                      /> 
+              {touched.terms && errors.terms && <p className="error">{errors.terms}</p>}
               <Field name="terms" 
                      type="checkbox"  
                      /> 
@@ -35,5 +49,21 @@ export default withFormik({
                      password: values.password || '',
                      terms: values.terms || false
               }
+       },
+       validationSchema: yup.object().shape({
+              name: yup.string().required("Name is required."),
+              email: yup.string().required("Email is required."),
+              password: yup.string().required("Password is required."),
+              terms: yup.boolean().oneOf([true], "You must agree to the terms & conditions.")
+       }),
+       handleSubmit: (values, {setStatus}) => {
+              axios.post('https://reqres.in/api/users', values)
+              .then(response => {
+                     setStatus(response.data)
+                     console.log("res", response.data)
+              })
+              .catch(err => {
+                     console.log("Error:", err)
+              })
        }
 })(MemberForm);
